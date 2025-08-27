76789e3de4f69e4ace1f81fdb3b764f2c996de16
rlagusalsb <kkhm0929@gmail.com>
2025-08-27 22:03:22 +0900

    feat: Comment 엔티티 추가

---
+++ backend/src/main/java/com/killseat/comment/entity/Comment.java
@@ -0,0 +1,55 @@
+package com.killseat.comment.entity;
+
+import com.killseat.member.entity.Member;
+import com.killseat.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Builder
+    private Comment(Member member, Post post, Comment parent, String content) {
+        this.member = member;
+        this.post = post;
+        this.parent = parent;
+        this.content = content;
+    }
+}
