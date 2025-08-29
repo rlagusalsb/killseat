e4ee7f25cc4ff721669e37c9808934dbdb3c71d6
rlagusalsb <kkhm0929@gmail.com>
2025-08-30 04:49:09 +0900

    feat: Show 엔티티 구현

---
+++ backend/src/main/java/com/killseat/show/entity/Show.java
@@ -0,0 +1,40 @@
+package com.killseat.show.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "show")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Show {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long showId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Builder
+    private Show(String title, LocalDateTime startTime, LocalDateTime endTime) {
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
