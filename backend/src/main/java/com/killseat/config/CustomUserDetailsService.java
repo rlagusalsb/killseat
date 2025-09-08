7cbe721599409d15588e25505b5278a79903cf29
rlagusalsb <kkhm0929@gmail.com>
2025-09-09 02:39:46 +0900

    feat: CustomUserDetailsService 구현

---
+++ backend/src/main/java/com/killseat/config/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.killseat.config;
+
+import com.killseat.member.entity.Member;
+import com.killseat.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/killseat/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.killseat.member.repository;
+
+import com.killseat.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
