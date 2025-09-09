03606d5890aea5cc75c09014fd01cc919be820e7
rlagusalsb <kkhm0929@gmail.com>
2025-09-09 21:13:30 +0900

    feat: MemberService 구현

---
+++ backend/src/main/java/com/killseat/member/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.killseat.member.service;
+
+import com.killseat.member.dto.SignupRequestDto;
+import com.killseat.member.entity.Member;
+import com.killseat.member.entity.Role;
+import com.killseat.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public boolean checkEmailDuplicate(String email) {
+        return memberRepository.findByEmail(email).isPresent();
+    }
+
+    @Transactional
+    public void signup(SignupRequestDto request) {
+        Member member = Member.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .name(request.getName())
+                .role(Role.USER)
+                .build();
+
+        memberRepository.save(member);
+    }
+}
