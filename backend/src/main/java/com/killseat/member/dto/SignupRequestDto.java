78e8783c27fb4509f2b8944ca0deea5b4069c6a3
rlagusalsb <kkhm0929@gmail.com>
2025-09-10 18:16:25 +0900

    refator: 검증 어노테이션 추가

--- backend/src/main/java/com/killseat/member/dto/SignupRequestDto.java
+++ backend/src/main/java/com/killseat/member/dto/SignupRequestDto.java
@@ -1,14 +1,23 @@
 package com.killseat.member.dto;
 
-import com.killseat.member.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class SignupRequestDto {
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Size(min = 2, max = 10, message = "이름은 2자 이상 10자 이하로 입력해주세요.")
     private String name;
-    private Role role;
 }
