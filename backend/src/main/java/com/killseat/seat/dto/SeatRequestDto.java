51a72d052d046145c09e7278aad721d4706a669a
rlagusalsb <kkhm0929@gmail.com>
2025-09-15 23:41:02 +0900

    feat: DTO 구현

---
+++ backend/src/main/java/com/killseat/seat/dto/SeatRequestDto.java
@@ -0,0 +1,10 @@
+package com.killseat.seat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SeatRequestDto {
+    private String seatNumber;
+}
