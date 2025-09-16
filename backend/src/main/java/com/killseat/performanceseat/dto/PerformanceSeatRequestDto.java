89d06c84ad8430c46842a6a633e5848e51e49874
rlagusalsb <kkhm0929@gmail.com>
2025-09-16 23:48:21 +0900

    feat: DTO 구현

---
+++ backend/src/main/java/com/killseat/performanceseat/dto/PerformanceSeatRequestDto.java
@@ -0,0 +1,11 @@
+package com.killseat.performanceseat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PerformanceSeatRequestDto {
+    private Long performanceId;
+    private Long seatId;
+}
