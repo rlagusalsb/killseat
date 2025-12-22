e5ea64e8ca5321488863f0cf8863a6e567316e52
rlagusalsb <kkhm0929@gmail.com>
2025-12-23 06:51:21 +0900

    feat: PaymentRepository 추가

---
+++ backend/src/main/java/com/killseat/payment/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.killseat.payment.repository;
+
+import com.killseat.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByMerchantUid(String merchantUid);
+}
