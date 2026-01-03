776a234bc271f444b8ecf830a37981780f34db5b
rlagusalsb <kkhm0929@gmail.com>
2026-01-04 07:43:09 +0900

    chore: axios 초기화 적용

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './css/Common.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "./css/Common.css";
+import { setupInterceptors } from "./api/setupInterceptors";
 
-createRoot(document.getElementById('root')).render(
+setupInterceptors();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
