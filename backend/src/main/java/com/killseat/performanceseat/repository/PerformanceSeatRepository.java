2bed54e615fef43df769a6bc1235d33a53f849eb
rlagusalsb <kkhm0929@gmail.com>
2026-01-05 06:52:30 +0900

    feat: 중복요청 방지를 위한 락 추가

--- backend/src/main/java/com/killseat/performanceseat/repository/PerformanceSeatRepository.java
+++ backend/src/main/java/com/killseat/performanceseat/repository/PerformanceSeatRepository.java
@@ -1,10 +1,19 @@
 package com.killseat.performanceseat.repository;
 
 import com.killseat.performanceseat.entity.PerformanceSeat;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PerformanceSeatRepository extends JpaRepository<PerformanceSeat, Long> {
     List<PerformanceSeat> findByPerformance_PerformanceId(Long performanceId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ps from PerformanceSeat ps where ps.performanceSeatId = :id")
+    Optional<PerformanceSeat> findByIdForUpdate(@Param("id") Long id);
 }
