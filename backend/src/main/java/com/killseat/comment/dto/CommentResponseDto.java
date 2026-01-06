4d257c2a6a0c88c2238d24f3381b4b58e4dea59e
rlagusalsb <kkhm0929@gmail.com>
2026-01-07 04:13:49 +0900

    feat: DTO 구현

---
+++ backend/src/main/java/com/killseat/comment/dto/CommentResponseDto.java
@@ -0,0 +1,21 @@
+package com.killseat.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CommentResponseDto {
+    private Long commentId;
+    private Long postId;
+    private Long memberId;
+    private String memberName;
+    private Long parentId;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<CommentResponseDto> children;
+}
---
+++ backend/src/main/java/com/killseat/comment/dto/CommentUpdateRequestDto.java
@@ -0,0 +1,10 @@
+package com.killseat.comment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CommentUpdateRequestDto {
+    private String content;
+}
