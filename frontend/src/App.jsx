ba5dd782ee0e95843009949e729fe4d7787c4bf2
rlagusalsb <kkhm0929@gmail.com>
2026-01-08 08:15:45 +0900

    feat: 마이페이지 라우트 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import MyReservation from "./pages/Reservations";
+import MyReservation from "./pages/MyReservations";
 import Login from "./pages/Login";
 import Performances from "./pages/Performance";
 import PerformanceSeats from "./pages/PerformanceSeats";
