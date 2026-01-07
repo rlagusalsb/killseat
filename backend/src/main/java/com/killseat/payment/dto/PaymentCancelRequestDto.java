57494b054f0e0651698b26558d9a3464e83311c8
rlagusalsb <kkhm0929@gmail.com>
2026-01-08 06:24:41 +0900

    feat: 애노테이션 추가

--- backend/src/main/java/com/killseat/payment/dto/PaymentCancelRequestDto.java
+++ backend/src/main/java/com/killseat/payment/dto/PaymentCancelRequestDto.java
@@ -1,11 +1,13 @@
 package com.killseat.payment.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentCancelRequestDto {
     @NotNull
     private Long paymentId;
