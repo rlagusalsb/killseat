3716165009fb39b8b07a32c5e3c6b7502d44c97f
rlagusalsb <kkhm0929@gmail.com>
2026-01-09 05:18:54 +0900

    feat: 스케줄링 애노테이션 적용

--- backend/src/main/java/com/killseat/KillseatApplication.java
+++ backend/src/main/java/com/killseat/KillseatApplication.java
@@ -2,7 +2,9 @@ package com.killseat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class KillseatApplication {
 
