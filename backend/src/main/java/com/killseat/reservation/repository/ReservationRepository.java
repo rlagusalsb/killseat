d5705ad85463665c6008c9d4ef6714e1fdf8ca99
rlagusalsb <kkhm0929@gmail.com>
2026-01-09 09:49:51 +0900

    feat: 결제 준비 추가

--- backend/src/main/java/com/killseat/reservation/repository/ReservationRepository.java
+++ backend/src/main/java/com/killseat/reservation/repository/ReservationRepository.java
@@ -13,8 +13,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    List<Reservation> findAllByMember_MemberId(Long memberId);
-
     @Query("""
         select r
           from Reservation r
@@ -26,7 +24,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     """)
     Reservation findDetailById(@Param("id") Long reservationId);
 
-    //상태 조건부 변경
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("""
         update Reservation r
@@ -40,15 +37,16 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
                             @Param("to") ReservationStatus to,
                             @Param("now") LocalDateTime now);
 
-    //만료된 PENDING 예약 조회
+    //만료된 PENDING/PAYING 예약 조회
     @Query("""
         select r
           from Reservation r
           join fetch r.performanceSeat ps
-         where r.status = com.killseat.reservation.entity.ReservationStatus.PENDING
+         where r.status in (com.killseat.reservation.entity.ReservationStatus.PENDING,
+                            com.killseat.reservation.entity.ReservationStatus.PAYING)
            and r.expiresAt < :now
     """)
-    List<Reservation> findExpiredPendings(@Param("now") LocalDateTime now);
+    List<Reservation> findExpiredHoldOrPaying(@Param("now") LocalDateTime now);
 
     @Query(value = """
         select r
@@ -71,4 +69,13 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("excludedStatus") ReservationStatus excludedStatus,
             Pageable pageable
     );
+
+    @Query("""
+        select r
+          from Reservation r
+          join fetch r.performanceSeat ps
+          join fetch ps.performance p
+         where r.reservationId = :id
+    """)
+    Reservation findForPaymentPrepare(@Param("id") Long reservationId);
 }
