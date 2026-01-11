39b1e610169cc7cb969c8bea85ef03af97c8cefa
rlagusalsb <kkhm0929@gmail.com>
2026-01-12 05:27:43 +0900

    fix: 역직렬화 오류 해결을 위한 서비스 코드 수정

--- backend/src/main/java/com/killseat/performance/service/PerformanceService.java
+++ backend/src/main/java/com/killseat/performance/service/PerformanceService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +26,7 @@ public class PerformanceService {
     public List<PerformanceResponseDto> getAll() {
         return performanceRepository.findAll().stream()
                 .map(performanceMapper::toDto)
-                .toList();
+                .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
