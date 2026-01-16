04aebe0b42872281d4131e0f1fa51c5699c88318
rlagusalsb <kkhm0929@gmail.com>
2026-01-17 04:08:51 +0900

    feat: 인증 관련 에러 코드 추가

--- backend/src/main/java/com/killseat/common/exception/CustomErrorCode.java
+++ backend/src/main/java/com/killseat/common/exception/CustomErrorCode.java
@@ -7,6 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CustomErrorCode {
+
+    //회원,인증 관련 에러 (M)
+    MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M001", "아이디 혹은 비밀번호를 확인해주세요"),
+    TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "M003", "로그인이 인증되지 않았습니다");
+
     private final HttpStatus status;
     private final String errorCode;
     private final String message;
