843e1235ba8dc045ddb5e8de24726059be31dfe9
rlagusalsb <kkhm0929@gmail.com>
2026-01-18 03:52:46 +0900

    feat: PerformanceRepository에 메서드 추가

--- backend/src/main/java/com/killseat/performance/repository/PerformanceRepository.java
+++ backend/src/main/java/com/killseat/performance/repository/PerformanceRepository.java
@@ -1,7 +1,11 @@
 package com.killseat.performance.repository;
 
 import com.killseat.performance.entity.Performance;
+import com.killseat.performance.entity.PerformanceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PerformanceRepository extends JpaRepository<Performance, Long> {
+    List<Performance> findAllByStatus(PerformanceStatus status);
 }
