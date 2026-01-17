09412fe84981c92598d0df6eb00b4432f54df50a
rlagusalsb <kkhm0929@gmail.com>
2026-01-18 02:34:48 +0900

    fix: Builder 애노테이션 추가

--- backend/src/main/java/com/killseat/admin/member/MemberResponseDto.java
+++ backend/src/main/java/com/killseat/admin/member/MemberResponseDto.java
@@ -2,11 +2,13 @@ package com.killseat.admin.member;
 
 import com.killseat.member.entity.Role;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class MemberResponseDto {
     private Long memberId;
