cdc50757ff4414e54fd8e1f732be68a9519d0427
rlagusalsb <kkhm0929@gmail.com>
2026-01-18 02:34:58 +0900

    feat: 멤버 조회용 컨트롤러 구현

---
+++ backend/src/main/java/com/killseat/admin/member/AdminMemberController.java
@@ -0,0 +1,29 @@
+package com.killseat.admin.member;
+
+import com.killseat.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/members")
+@RequiredArgsConstructor
+public class AdminMemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping
+    public ResponseEntity<Page<MemberResponseDto>> getAllMembers(
+            @PageableDefault(size = 10, sort = "memberId", direction = Sort.Direction.DESC) Pageable pageable
+    )
+    {
+        Page<MemberResponseDto> members = memberService.getAllMembers(pageable);
+        return ResponseEntity.ok(members);
+    }
+}
