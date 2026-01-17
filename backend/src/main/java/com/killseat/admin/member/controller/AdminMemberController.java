7414a0ded721c05790db4a158e510888f04139a6
rlagusalsb <kkhm0929@gmail.com>
2026-01-18 04:50:42 +0900

    feat: ADMIN 애노테이션 추가

--- backend/src/main/java/com/killseat/admin/member/controller/AdminMemberController.java
+++ backend/src/main/java/com/killseat/admin/member/controller/AdminMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/admin/members")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminMemberController {
 
     private final MemberService memberService;
