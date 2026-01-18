c05b1f7859a1a74d89d3371e4787aed2d0dd893f
rlagusalsb <kkhm0929@gmail.com>
2026-01-19 08:23:55 +0900

    feat: 애노테이션 추가

--- backend/src/main/java/com/killseat/performance/dto/ScheduleResponseDto.java
+++ backend/src/main/java/com/killseat/performance/dto/ScheduleResponseDto.java
@@ -2,10 +2,12 @@ package com.killseat.performance.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ScheduleResponseDto {
     private Long scheduleId;
