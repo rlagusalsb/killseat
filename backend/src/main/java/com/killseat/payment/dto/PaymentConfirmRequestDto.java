52535073f29809759c8acd811d30042280a023a1
rlagusalsb <kkhm0929@gmail.com>
2026-01-21 05:10:45 +0900

    feat: AllArgsConstructor 애노테이션 추가

--- backend/src/main/java/com/killseat/payment/dto/PaymentConfirmRequestDto.java
+++ backend/src/main/java/com/killseat/payment/dto/PaymentConfirmRequestDto.java
@@ -1,11 +1,13 @@
 package com.killseat.payment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentConfirmRequestDto {
     @NotBlank
     private String merchantUid;
--- backend/src/main/java/com/killseat/payment/dto/PaymentPrepareRequestDto.java
+++ backend/src/main/java/com/killseat/payment/dto/PaymentPrepareRequestDto.java
@@ -2,11 +2,13 @@ package com.killseat.payment.dto;
 
 import com.killseat.payment.entity.PaymentMethod;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentPrepareRequestDto {
     @NotNull
     private Long reservationId;
