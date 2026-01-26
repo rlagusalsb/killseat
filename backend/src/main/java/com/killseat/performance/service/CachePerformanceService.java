1c7b26b23411bdc3c5a812cb8d5f9faf5b91aa0d
rlagusalsb <kkhm0929@gmail.com>
2026-01-27 06:22:18 +0900

    feat: 공연 목록 조회 메서드 페이징 처리로 변경 (cache)

--- backend/src/main/java/com/killseat/performance/service/CachePerformanceService.java
+++ backend/src/main/java/com/killseat/performance/service/CachePerformanceService.java
@@ -16,6 +16,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,12 +35,11 @@ public class CachePerformanceService implements PerformanceService {
 
     //OPEN 상태인 공연들만 조회
     @Override
-    @Cacheable(value = "performanceList", key = "'all'")
+    @Cacheable(value = "performanceList", key = "#pageable.pageNumber")
     @Transactional(readOnly = true)
-    public List<PerformanceResponseDto> getActivePerformances() {
-        return performanceRepository.findAllByStatus(PerformanceStatus.OPEN).stream()
-                .map(performanceMapper::toDto)
-                .collect(Collectors.toList());
+    public Page<PerformanceResponseDto> getActivePerformances(Pageable pageable) {
+        return performanceRepository.findAllByStatus(PerformanceStatus.OPEN, pageable)
+                .map(performanceMapper::toDto);
     }
 
     //공연 상세 조회
