01300cd19f6538abdb6e3535ae54b4504c623c60
rlagusalsb <kkhm0929@gmail.com>
2026-01-27 06:20:53 +0900

    feat: 페이징 처리 추가

--- backend/src/main/java/com/killseat/performance/repository/PerformanceRepository.java
+++ backend/src/main/java/com/killseat/performance/repository/PerformanceRepository.java
@@ -2,10 +2,10 @@ package com.killseat.performance.repository;
 
 import com.killseat.performance.entity.Performance;
 import com.killseat.performance.entity.PerformanceStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PerformanceRepository extends JpaRepository<Performance, Long> {
-    List<Performance> findAllByStatus(PerformanceStatus status);
+    Page<Performance> findAllByStatus(PerformanceStatus status, Pageable pageable);
 }
