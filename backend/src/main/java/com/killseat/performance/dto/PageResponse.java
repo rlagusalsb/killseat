f3df472f8f95c1f0db7664b4a540ae0b1ff6ff20
rlagusalsb <kkhm0929@gmail.com>
2026-01-28 06:21:14 +0900

    feat: 공연목록 조회용 DTO 구현

---
+++ backend/src/main/java/com/killseat/performance/dto/PageResponse.java
@@ -0,0 +1,27 @@
+package com.killseat.performance.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+
+    public PageResponse(Page<T> page) {
+        this.content = page.getContent();
+        this.pageNumber = page.getNumber();
+        this.pageSize = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+    }
+}
