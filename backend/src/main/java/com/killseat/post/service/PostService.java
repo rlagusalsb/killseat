cdf62074cb75959fbd5becf0f75ace02727dfd4d
rlagusalsb <kkhm0929@gmail.com>
2026-03-09 18:32:35 +0900

    fix: 불필요한 import 제거

--- backend/src/main/java/com/killseat/post/service/PostService.java
+++ backend/src/main/java/com/killseat/post/service/PostService.java
@@ -12,10 +12,8 @@ import com.killseat.post.service.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 @RequiredArgsConstructor
